72124ace5ce22efb22e39fb605ebd22b7c8eb503
srewashimondal <mondalsrewashi2004@gmail.com>
2025-07-09 23:23:14 -0400

    Added timeline page with fetching

--- app/routes.py
+++ app/routes.py
@@ -142,3 +142,7 @@ def get_time_line_post():
             for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
         ]
     }
+
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline")
