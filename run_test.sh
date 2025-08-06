60175d1726cd242cfafc56ded6335e55488fe36a
srewashimondal <mondalsrewashi2004@gmail.com>
2025-08-05 20:51:36 -0400

    Fix: changed to python instead of python3 and added venv and changed run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-$PWD/venv/Scripts/python.exe -m unittest discover -v tests/
+set -e
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+  $PWD/venv/Scripts/python.exe -m unittest discover -v tests/
+else
+  $PWD/venv/bin/python -m unittest discover -v tests/
+fi
+
 
 
